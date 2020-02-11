c0caa09c88fdacd6a55ff29fe6b50d308824ae62
daltonkell <Daltonkell@rpsgroup.com>
2020-02-11 20:31:35 +0000

    Configure auto-deployment using Travis-CI for the master repo and branch
    On merges into the master branch, the Docker image is pushed to Dockerhub
    with the ":latest" tag. On releases, the Docker image is tagged with the
    release tag and pushed to Dockerhub.

---
+++ scripts/docker_push.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Dockerhub login
+[ ! -z "$DOCKER_PASSWORD" ] || [ ! -z "$DOCKER_USERNAME" ];
+  echo "DOCKER_USERNAME and DOCKER_PASSWORD must be set as environment variables!";
+  exit;
+echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+# if the build is for a pushed tag, use that tag for the Docker image;
+# otherwise, use "latest"
+if [[ -z "$TRAVIS_TAG" ]]; then
+  image_release_tag="latest";
+else
+  image_release_tag="$TRAVIS_TAG";
+fi
+
+echo "Tagging image with: $image_release_tag"
+docker tag "ioos-us:latest" "rpsdevopsdocker/ioos-us:$image_release_tag"
+docker push "rpsdevopsdocker/ioos-us:$image_relase_tag"
