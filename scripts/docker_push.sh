83485deb724c3dc36a8715c69e4a2803f3de9c5d
daltonkell <Daltonkell@rpsgroup.com>
2020-02-12 14:03:24 +0000

    Tag image appropriately

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -15,5 +15,5 @@ else
 fi
 
 echo "Tagging image with: $image_release_tag"
-docker tag "ioos-us:latest" "rpsdevopsdocker/ioos-us:$image_release_tag"
+docker tag "ioos-us:latest" "ioos-us/ioos-us:$image_release_tag"
 docker push "ioos/ioos-us:$image_relase_tag"
