959e356aa5bc78727578599354786cf7e64bbfcb
daltonkell <daltonkell13@gmail.com>
2020-02-17 13:59:58 +0000

    Clean up BASH script for Dockerhub push

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 # Dockerhub login
-[ ! -z "$DOCKER_PASSWORD" ] || [ ! -z "$DOCKER_USERNAME" ];
-  echo "DOCKER_USERNAME and DOCKER_PASSWORD must be set as environment variables!";
-  exit 1;
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-
-# if the build is for a pushed tag, use that tag for the Docker image;
-# otherwise, use "latest"
-if [[ -z "$TRAVIS_TAG" ]]; then
-  image_release_tag="latest";
+if [[ ! -n "$DOCKER_PASSWORD" || ! -n "$DOCKER_USERNAME" ]] >&2; then
+  echo "DOCKER_USERNAME and DOCKER_PASSWORD must be set as environment variables!"
+  exit 1
 else
-  image_release_tag="$TRAVIS_TAG";
-fi
+  docker login -u "$DOCKER_USERNAME" --password-stdin <<< "$DOCKER_PASSWORD" >&2
 
-echo "Tagging image with: $image_release_tag"
-docker tag "ioos-us:latest" "ioos-us/ioos-us:$image_release_tag"
-docker push "ioos/ioos-us:$image_relase_tag"
+  if [[ $? -ne 0 ]]; then
+    exit 1
+  fi
+  
+  # if the build is for a pushed tag, use that tag for the Docker image;
+  # otherwise, use "latest (via BASH substitution)"
+  image_release_tag="${TRAVIS_TAG:-latest}"
+  
+  echo "Tagging image with: $image_release_tag"
+  docker tag "ioos-us:latest" "ioos-us/ioos-us:$image_release_tag"
+  docker push "ioos/ioos-us:$image_relase_tag"
+fi
